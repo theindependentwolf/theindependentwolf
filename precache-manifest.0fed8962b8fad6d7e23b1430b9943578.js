19b1400c187db5eac335de252e666fabcbf17db3
theindependentwolf <aniruth.oblah@gmail.com>
2020-05-22 01:04:31 -0400

    Updates

---
+++ precache-manifest.0fed8962b8fad6d7e23b1430b9943578.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715214147447c0f48019b5174f7199f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a5b83ff1e964ee71804",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7585aefb6cf44feda188",
+    "url": "/static/css/main.a24beb77.chunk.css"
+  },
+  {
+    "revision": "9a5b83ff1e964ee71804",
+    "url": "/static/js/2.823b095b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.823b095b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7585aefb6cf44feda188",
+    "url": "/static/js/main.bb74a4d9.chunk.js"
+  },
+  {
+    "revision": "bfc4b6cdcfefcec97696",
+    "url": "/static/js/runtime-main.bfc7e39d.js"
+  }
+]);

f777927e7c400470820e29cf354a16dde268ebd9
theindependentwolf <aniruth.oblah@gmail.com>
2020-05-22 01:01:30 -0400

    Updates

---
+++ precache-manifest.e4fd5d7c4610e544e2547777b64d67b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e2461e80ab13c0c6cad6951f22ad70",
+    "url": "/theindependentwolf/index.html"
+  },
+  {
+    "revision": "9a5b83ff1e964ee71804",
+    "url": "/theindependentwolf/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7ac4f87c75ca4f8343f8",
+    "url": "/theindependentwolf/static/css/main.a24beb77.chunk.css"
+  },
+  {
+    "revision": "9a5b83ff1e964ee71804",
+    "url": "/theindependentwolf/static/js/2.823b095b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/theindependentwolf/static/js/2.823b095b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac4f87c75ca4f8343f8",
+    "url": "/theindependentwolf/static/js/main.740efe07.chunk.js"
+  },
+  {
+    "revision": "2f8299208fee53a69211",
+    "url": "/theindependentwolf/static/js/runtime-main.6366aa0a.js"
+  }
+]);
